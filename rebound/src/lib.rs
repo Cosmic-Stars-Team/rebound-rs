f3f229e8f88a707106931c9cf05b69cdfca9cc49
yuanzui-cf <im.leojia@gmail.com>
2026-03-27 22:57:59 +0800

    feat(particles): Add Particle struct

--- rebound/src/lib.rs
+++ rebound/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod error;
+pub mod particles;
 pub mod simulator;
 pub mod utils;
---
+++ rebound/src/particles/mod.rs
@@ -0,0 +1,38 @@
+use crate::simulator::Simulation;
+use rebound_bind as rb;
+
+pub type ParticlePosition = (f64, f64, f64);
+
+pub struct Particle {
+    pub mess: f64,
+    pub position: ParticlePosition,
+    pub velocity: ParticlePosition,
+}
+
+impl From<Particle> for rb::reb_particle {
+    fn from(particle: Particle) -> Self {
+        rebound_bind::reb_particle {
+            x: particle.position.0,
+            y: particle.position.1,
+            z: particle.position.2,
+            vx: particle.velocity.0,
+            vy: particle.velocity.1,
+            vz: particle.velocity.2,
+            ax: 0.0,
+            ay: 0.0,
+            az: 0.0,
+            m: particle.mess,
+            r: 0.0,
+            last_collision: 0.0,
+            c: std::ptr::null_mut(),
+            hash: 0,
+            ap: std::ptr::null_mut(),
+            sim: std::ptr::null_mut(),
+        }
+    }
+}
+
+// pub struct ParticleRef<'a> {
+//     pub(crate) inner: Option<*mut rb::reb_particle>,
+//     pub(crate) _sim: &'a Simulation,
+// }
