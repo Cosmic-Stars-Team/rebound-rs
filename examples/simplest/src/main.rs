c743673d975f9a7e0ca8665a39d5d3e0d9dffa0c
yuanzui-cf <im.leojia@gmail.com>
2026-04-21 01:16:40 +0800

    feat(examples): Add simple workspace example

---
+++ examples/simplest/src/main.rs
@@ -0,0 +1,46 @@
+use rebound::{Result, create_particle, simulator::Simulation};
+
+fn main() -> Result<()> {
+    let mut r = Simulation::new()
+        .add_particle(create_particle! {
+            mass: 1.
+        })?
+        .add_particle(create_particle! {
+            mass: 1e-3,
+            a: 1.,
+            e: 0.1,
+        })?
+        .add_particle(create_particle! {
+            a: 1.4,
+            e: 0.1,
+        })?;
+
+    r.integrate(100.)?;
+
+    for particle in r.particles() {
+        let pos = particle
+            .position()
+            .ok_or_else(|| rebound::Error::Custom("Particle position not found".into()))?;
+
+        println!("{} {} {}", pos.0, pos.1, pos.2);
+    }
+
+    let primary = r
+        .get_particle(0)
+        .ok_or_else(|| rebound::Error::Custom("Primary particle not found".into()))?;
+
+    for i in 1..r.n() {
+        let orbit = r
+            .get_particle(i)
+            .ok_or_else(|| rebound::Error::Custom("Particle orbit not found".into()))?
+            .into_orbit(r.g(), &primary)
+            .ok_or_else(|| rebound::Error::Custom("Particle orbit not found".into()))?;
+
+        println!(
+            "{} {} {}",
+            orbit.semi_major_axis, orbit.eccentricity, orbit.true_anomaly
+        );
+    }
+
+    Ok(())
+}
