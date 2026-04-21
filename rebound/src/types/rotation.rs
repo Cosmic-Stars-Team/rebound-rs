6a56c9fc7fff04aa4c1e49d6f5eb4a8162dd5c81
yuanzui-cf <im.leojia@gmail.com>
2026-04-21 17:05:06 +0800

    feat(rotation): Add quaternion math helpers and mul

--- rebound/src/types/rotation.rs
+++ rebound/src/types/rotation.rs
@@ -1,3 +1,5 @@
+use std::ops::Mul;
+
 use rebound_bind as rb;
 
 use crate::types::Vec3d;
@@ -11,6 +13,45 @@ pub struct Rotation {
 }
 
 impl Rotation {
+    pub fn inverse(self) -> Self {
+        let scale = 1.0 / self.length_squared();
+        let conjugate = self.conjugate();
+        Self {
+            ix: conjugate.ix * scale,
+            iy: conjugate.iy * scale,
+            iz: conjugate.iz * scale,
+            r: conjugate.r * scale,
+        }
+    }
+
+    pub fn identity() -> Self {
+        Self {
+            ix: 0.0,
+            iy: 0.0,
+            iz: 0.0,
+            r: 1.0,
+        }
+    }
+
+    pub fn normalize(self) -> Self {
+        let scale = 1.0 / self.length_squared().sqrt();
+        Self {
+            ix: self.ix * scale,
+            iy: self.iy * scale,
+            iz: self.iz * scale,
+            r: self.r * scale,
+        }
+    }
+
+    pub fn conjugate(self) -> Self {
+        Self {
+            ix: -self.ix,
+            iy: -self.iy,
+            iz: -self.iz,
+            r: self.r,
+        }
+    }
+
     pub fn init_from_to(from: Vec3d, to: Vec3d) -> Self {
         unsafe { rb::reb_rotation_init_from_to(from.into(), to.into()) }.into()
     }
@@ -44,6 +85,27 @@ impl Rotation {
 
     pub fn imag(self) -> Vec3d {
         Vec3d(self.ix, self.iy, self.iz)
+    }
+
+    fn dot(self, other: Self) -> f64 {
+        self.r * other.r + self.ix * other.ix + self.iy * other.iy + self.iz * other.iz
+    }
+
+    fn length_squared(self) -> f64 {
+        self.dot(self)
+    }
+}
+
+impl Mul<Rotation> for Rotation {
+    type Output = Self;
+
+    fn mul(self, rhs: Rotation) -> Self::Output {
+        Rotation {
+            r: self.r * rhs.r - self.ix * rhs.ix - self.iy * rhs.iy - self.iz * rhs.iz,
+            ix: self.r * rhs.ix + self.ix * rhs.r + self.iy * rhs.iz - self.iz * rhs.iy,
+            iy: self.r * rhs.iy - self.ix * rhs.iz + self.iy * rhs.r + self.iz * rhs.ix,
+            iz: self.r * rhs.iz + self.ix * rhs.iy - self.iy * rhs.ix + self.iz * rhs.r,
+        }
     }
 }
 
